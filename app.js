8561e92e16f53bfb8c00d1e80f7115176eef0ab5
Andrew James <andrewpjames87@gmail.com>
2015-05-18 09:55:18 -0600

    Added span for forest background and backgroundToggle functionality

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
 
-	$('span.dark').on('click', function(){
-		$('body').css('background-image','url(img/dark_wood.png)');
-	});
+	var backgroundToggle = function(){
+		$('span.dark').on('click', function(){
+			$('body').css('background-image','url(img/dark_wood.png)');
+		});
 
-	$('span.light').on('click', function(){
-		$('body').css('background-image','url(img/tileable_wood_texture.png)');
-	});
+		$('span.light').on('click', function(){
+			$('body').css('background-image','url(img/tileable_wood_texture.png)');
+		});
 
+		$('span.forest').on('click', function(){
+			$('body').css({'background-image':'url(img/forest.jpg)', 'background-size': 'cover'});
+		});
+	};
+
+	backgroundToggle();
 });
