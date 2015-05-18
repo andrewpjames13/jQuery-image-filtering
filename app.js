26b2251358e8a8034383e203b25e9d524534964c
Andrew James <andrewpjames87@gmail.com>
2015-05-18 09:49:12 -0600

    User clicks on the light wood button and the back ground changes

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
 
 	$('span.dark').on('click', function(){
-		console.log('hi');
 		$('body').css('background-image','url(img/dark_wood.png)');
+	});
+
+	$('span.light').on('click', function(){
+		$('body').css('background-image','url(img/tileable_wood_texture.png)');
 	});
 
 });
